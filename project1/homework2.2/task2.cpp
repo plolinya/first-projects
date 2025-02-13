7a4d7e870b2d9065f33c85e7330c16daafbfa9cb
plolinya <polinar1102@gmail.com>
2025-02-13 23:40:31 +0200

    hw 22

---
+++ project1/homework2.2/task2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "please enter two numbers:";
+	cin >> a >> b;
+	cout << a + b << endl << a - b << endl << a * b << endl << a / b << endl << b / a << endl << a%b << endl << b%a;
+}

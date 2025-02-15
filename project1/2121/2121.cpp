4d88b92463e0b0bb9fd1224e4877e4a501ab95e7
plolinya <polinar1102@gmail.com>
2025-02-15 22:21:12 +0200

    przavd

---
+++ project1/2121/2121.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    int a; 
+    cout << "enter a\t"; 
+    cin >> a; 
+    if (a == 0)
+    {
+        cout << "nol yomayo";
+    }
+    else if (a % 2 == 0)
+    {
+        cout << "parne";
+    }
+    else if (a % 2 == 1)
+    {
+        cout << "neparne";
+    }
+}

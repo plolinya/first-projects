0c9e4060261b2ee8ac2a6430005dd1c34026cabc
plolinya <polinar1102@gmail.com>
2025-02-28 06:58:39 +0200

    domeeeeshk

---
+++ project1/calc1/calc1.cpp
@@ -0,0 +1,74 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, result;
+	char o; 
+	float resultdivision;
+	cout << "please enter number 1:\t";
+	cin >> num1;
+	cout << "please enter an operation:\t";
+	cin >> o;
+	cout << "please enter number 2:\t";
+	cin >> num2;
+	
+	if (o == '+')
+	{
+		result = num1 + num2;
+		cout << result;
+		if (result % 2 == 0)
+		{
+			cout << "\nogo! it's a parne chyslo";
+		}
+		if (result % 2 == 1)
+		{
+			cout << "\nogo! it's a neparne chyslo";
+		}
+	}
+	else if (o == '-')
+	{
+		result = num1 - num2;
+		cout << result;
+		if (result % 2 == 0)
+		{
+			cout << "\nogo! it's a parne chyslo";
+		}
+		if (result % 2 == 1)
+		{
+			cout << "\nogo! it's a neparne chyslo";
+		}
+	}
+	else if (o == '*')
+	{
+		result = num1 * num2;
+		cout << result;
+		if (result % 2 == 0)
+		{
+			cout << "\nogo! it's a parne chyslo";
+		}
+		if (result % 2 == 1)
+		{
+			cout << "\nogo! it's a neparne chyslo";
+		}
+	}
+	else if (o == '/')
+	{
+		resultdivision = (float)num1 / (float)num2;
+		cout << (float)resultdivision;
+		/*if (num2 == 0)
+		{
+			cout << "whoops! we can't do that at our math level :(";
+		}
+		}*/
+	}
+	else if (o == '%')
+	{
+		result = num1 % num2;
+		cout << result;
+	}
+	else {
+		cout << "\nwhoops! something is wrong";
+	}
+}
+	
---
+++ project1/calc2/calc2.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, result;
+	char o;
+	float resultdivision;
+	cout << "please enter number 1:\t";
+	cin >> num1;
+	cout << "please enter an operation:\t";
+	cin >> o;
+	cout << "please enter number 2:\t";
+	cin >> num2;
+	switch (o)
+	{
+	case '+':
+		result = num1 + num2;
+		cout << result;
+		if (result % 2 == 0)
+		{
+			cout << "\nogo! it's a parne chyslo";
+		}
+		if (result % 2 == 1)
+		{
+			cout << "\nogo! it's a neparne chyslo";
+		}
+		break;
+	case '-':
+		result = num1 - num2;
+		cout << result;
+		if (result % 2 == 0)
+		{
+			cout << "\nogo! it's a parne chyslo";
+		}
+		if (result % 2 == 1)
+		{
+			cout << "\nogo! it's a neparne chyslo";
+		}
+		break;
+	case '*':
+		result = num1 * num2;
+		cout << result;
+		if (result % 2 == 0)
+		{
+			cout << "\nogo! it's a parne chyslo";
+		}
+		if (result % 2 == 1)
+		{
+			cout << "\nogo! it's a neparne chyslo";
+		}
+		break;
+	case '/':
+		resultdivision = (float)num1 / (float)num2;
+		cout << (float)resultdivision;
+		break;
+	case '%':
+		result = num1 % num2;
+		cout << result;
+		break;
+	}
+}
